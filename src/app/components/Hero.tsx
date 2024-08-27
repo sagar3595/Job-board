5aeb584499f6e54fca438d05748172f57a09a6fc
sagar3595 <sagar47kumar13@gmail.com>
2024-08-28 00:35:39 +0530

    first commit

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,24 @@
+export default function Hero() {
+  return (
+    <section className="container my-16">
+      <h1 className="text-4xl font-bold text-center">
+        Find your next
+        <br />
+        dream job
+      </h1>
+      {/* <p className="text-center text-gray-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        maxime natus deleniti incidunt. Dolorum ullam cupiditate reprehenderit,
+        assumenda rec.
+      </p> */}
+      <form className="flex gap-2 mt-4 max-w-md mx-auto">
+        <input type="search"
+        className="border border-gray-400 w-full py-2 px-3 rounded-md"
+        placeholder="Search phrase ..." />
+        <button className="bg-blue-600 text-white py-2 px-4 rounded-md">
+            Search
+        </button>
+      </form>
+    </section>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+     screens: {
+      'lg': '768px',
+      'xl': '768px',
+      '2xl': '768px'
+     
+      
+      },
+    
+    center: true,
+    padding: {
+      DEFAULT : '1.5rem',
+    },
+  },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -16,5 +30,5 @@ const config: Config = {
     },
   },
   plugins: [],
-};
+}
 export default config;
